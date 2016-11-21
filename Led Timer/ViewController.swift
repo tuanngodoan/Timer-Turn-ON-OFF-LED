8f7d86d3f38f70b69a808fde86fd661525ce9dda
Doãn Tuấn <tuanngodoan@hotmail.com>
2016-11-21 09:09:16 +0700

    Initial Commit

---
+++ Led Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Led Timer
+//
+//  Created by Doãn Tuấn on 11/21/16.
+//  Copyright © 2016 doantuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
